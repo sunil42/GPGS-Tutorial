7f16d2c8f7e44207d044512bca601dcdb7c04049
Sunil Behera <sb1510@outlook.com>
2020-10-02 03:40:16 +0530

    Add files via upload

---
+++ Assets/GPGSManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+
+public class GPGSManager : MonoBehaviour
+{
+    public Text m_Message;
+    public Button m_SignIn;
+
+
+    private void Start()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+    .RequestIdToken()
+    .RequestServerAuthCode(false)
+    .Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+
+        m_SignIn.onClick.RemoveAllListeners();
+        m_SignIn.onClick.AddListener(SignInGooglePlayGames);
+
+        SignInGooglePlayGames();
+    }
+
+    private void SignInGooglePlayGames()
+    {
+        PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptAlways, (result) =>
+        {
+            m_Message.text = result.ToString();
+            m_SignIn.onClick.AddListener(SignoutGooglePlay);
+        });
+    }
+
+    private void SignoutGooglePlay()
+    {
+        PlayGamesPlatform.Instance.SignOut();
+        m_Message.text = "Sign Out";
+        SignInGooglePlayGames();
+    }
+}
